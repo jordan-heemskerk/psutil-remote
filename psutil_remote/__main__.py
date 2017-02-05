012c8913cf331631a091dabb2cbaf667f2291bd4
Jordan Heemskerk <jordanh@shaw.ca>
2017-02-04 23:02:10 -0800

    Add entrypoint

---
+++ psutil_remote/__main__.py
@@ -0,0 +1,19 @@
+import sys
+
+
+def main(args=None):
+    """
+    The main routine
+
+    :param args: Command line arguments
+    :type args: list(str)
+    :return: None
+    """
+    if args is None:
+        args = sys.argv[1:]
+
+    # do things
+
+
+if __name__ == "__main__":
+    main()
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import os
-from distutils.core import setup
+from setuptools import setup
 
 # initialize __version__
 execfile(os.path.join("psutil_remote", "version.py"))
@@ -13,4 +13,9 @@ setup(name='psutil-remote',
       author_email='jordanh@shaw.ca',
       url='https://github.com/jordan-heemskerk/psutil-remote',
       packages=['psutil_remote'],
+      entry_points={
+          'console_scripts': [
+              'psutil-remote = psutil_remote.__main__:main'
+          ]
+      },
      )
